28c389b0937531b7d6565602598b62a4d40f9dcf
米应如是 <mifazhan10@gmail.com>
2026-03-17 16:15:19 +0800

    feat(goroutines): 添加斐波那契数列生成器和死锁说明文档
    - 实现了基于通道的斐波那契数列生成函数
    - 添加了协程间通信的 select 语句示例
    - 创建了详细的死锁问题分析文档
    - 解释了 Go 协程执行顺序和阻塞机制
    - 提供了正确的并发编程实践指导

--- Day 2/Go/12-Select/main.go
+++ Day 2/Go/12-Select/main.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			// 返回 fibonacci 函数，结束函数执行
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	fibonacci(c, quit)
+}
